10fbce47ebdff715bfbb5a9fa1bb6db8bd03c24b
cola2bear <129868083+cola2bear@users.noreply.github.com>
2024-03-11 01:45:28 +0900

    Add files via upload
    test

---
+++ crud3/src/main/java/egovframework/example/main/model/MainVO.java
@@ -0,0 +1,19 @@
+package egovframework.example.main.model;
+
+public class MainVO {
+
+	private String BOARD_ID;
+	private String TITLE;
+	private String CONTENTS;
+	private int HIT_CNT;
+	private String CREATOR_ID;
+	private String CREATED_NM;
+	private String PASSWORD;
+	private String ATCH_FILE_ID;
+	private String FRST_REGIST_PNTTM;
+	private String FRST_REGISTER_ID;
+	private String LAST_UPDT_PNTTM;
+	private String LAST_UPDUSR_ID;
+	private String DELETED_YN;
+
+}
